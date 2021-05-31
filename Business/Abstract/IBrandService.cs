32753bf43ee148a30c3d64dc40a3cef3aa4f7f3f
serkan özel <serkanozel399@gmail.com>
2021-05-31 22:12:25 +0300

    Add project files.

---
+++ Business/Abstract/IBrandService.cs
@@ -0,0 +1,20 @@
+﻿using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Abstract
+{
+    public interface IBrandService
+    {
+        List<Brand> GetAll();
+        List<Brand> GetAllByBrandId(int id);
+
+        void Add(Brand brand);
+        void Update(Brand brand);
+        void Delete(Brand brand);
+
+    }
+}
---
+++ Business/Concrete/CarManager.cs
@@ -0,0 +1,62 @@
+﻿using Business.Abstract;
+using DataAccess.Abstract;
+using DataAccess.Concrete.InMemory;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Concrete
+{
+    public class CarManager : ICarService
+    {
+        ICarDal _carDal;
+
+        public CarManager(ICarDal carDal)
+        {
+            _carDal = carDal;
+        }
+
+        public void Add(Car car)
+        {
+            if (car.CarName.Length <= 2)
+            {
+                throw new Exception("Araç Adı iki haneden fazla olmalı");
+
+            }
+            else
+            {
+                _carDal.Add(car);
+            }
+        }
+
+        public void Delete(Car car)
+        {
+            _carDal.Delete(car);
+        }
+
+        public List<Car> GetAll()
+        {
+            return _carDal.GetAll();
+        }
+
+        public List<Car> GetAllById(int id)
+        {
+            return _carDal.GetAll(p => p.Id == id);
+        }
+
+
+
+        public List<Car> GetByDailyPrice(decimal min, decimal max)
+        {
+            return _carDal.GetAll(p => p.DailyPrice >= min && p.DailyPrice <= max);
+        }
+
+        public void Update(Car car)
+        {
+            _carDal.Update(car);
+        }
+    }
+}
